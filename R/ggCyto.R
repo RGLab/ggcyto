a03364bf9f1e6cc51883c88d8d23858d6b5c94aa
mikejiang <wjiang2@fhcrc.org>
2015-04-23 12:03:29 -0700

    add default 1d and 2d plot

--- R/ggCyto.R
+++ R/ggCyto.R
@@ -1,18 +1,39 @@
+#' @export
 fortify.flowFrame <- function(model, data, ...){
-  as.data.frame(exprs(model))
+  #covert to flowSet
+  sn <- identifier(model)
+  model <- as(model, "flowSet")
+  sampleNames(model) <- sn
+  pData(model)[["name"]] <- sn
+  #then dispatch to forityf method for flowSet
+  fortify(model, ...)
+}
+
+#' @export
+as.data.frame.flowFrame <- function(x, ...){
+  as.data.frame(exprs(x))
+}
+
+#' @export
+as.data.frame.flowSet <- function(x, ...){
+  df.list <- fsApply(x, as.data.frame, simplify = FALSE)
+  df <- ldply(df.list)
+  df  
 }
 
 #' @importFrom plyr ldply
+#' @export
 fortify.flowSet <- function(model, data, ...){
-  
-  df.list <- fsApply(model, fortify, simplify = FALSE)
+  #convert to data.frame
+  df <- as.data.frame(model)
+  #merge with pData
   pd <- pData(model)
-  df <- ldply(df.list)
   df <- merge(pd, df, by.x = "name", by.y = ".id")
   df
 }
 
-#' @export
+#' @importFrom ggplot2 autoplot
+#' @export autoplot.flowFrame
 autoplot.flowFrame <- function(object, ...){
   sn <- identifier(object)
   object <- as(object, "flowSet")
@@ -21,15 +42,48 @@ autoplot.flowFrame <- function(object, ...){
   autoplot(object, ...)
 }
 
-#' @export
-autoplot.flowSet <- function(object, ...){
+
+#' @importFrom RColorBrewer brewer.pal
+#' @export autoplot.flowSet
+autoplot.flowSet <- function(object, mapping, ..., plotType = "histogram", margin = TRUE){
+  
+  plotType <- match.arg(plotType, c("histogram", "density", "xyplot"))
+  
+  # check the dimensions
+  if(!missing(mapping)){
+    dims <- sapply(mapping,as.character)
+    nDims <- length(mapping)
+  }else
+    stop("mapping must be supplied to ggplot!")
+  
+  # apply boundary filter to remove outliers
+  if(margin){
+    g <- boundaryFilter(x = dims, tol = 1e-5)
+    object <- Subset(object, g)
+  }
+  
   df <- fortify(object)
   
-  p <- ggplot(df, ...)
-  p <- p + aes(fill = 1)
+  p <- ggplot(df, mapping, ...)
+  
+  #hide the legend by default
   p <- p + guides(colour = F, fill = F)
-
+  
+  #default faceting by sample names
   p <- p + facet_wrap(~name)
   
+  #if 2d
+  if(nDims == 1){
+    if(plotType == "histogram"){
+      p <- p + geom_histogram()
+    }else{
+      p <- p + geom_density(aes(fill = "grey50", colour = "grey50"))
+    }
+      
+  }else if(nDims == 2){
+    p <- p + geom_hex() 
+    p <- p + scale_fill_gradientn(colours = rev(brewer.pal(11, "Spectral")))  
+  }else
+    stop("Only 1d or 2d plots are currently supported!")
   p
 }
--- README.md
+++ README.md
@@ -5,52 +5,82 @@ date: "04/13/2015"
 output: html_document
 ---
 
-```{r, echo=FALSE}
-library(knitr)
-opts_chunk$set(message = FALSE, warning = FALSE)
-```
-
-`ggCyto` makes `ggplot2` to be able to work with `Cytometry` data, namely `flowSet/ncdfFlowSet` or `flowFrame` S4 objects.
 
 
-```{r}
+## `ggCyto` makes `ggplot2` to be able to work with `Cytometry` data, namely `flowSet/ncdfFlowSet` or `flowFrame` S4 objects.
+
+
+
+```r
 library(ggCyto)
 data(GvHD)
 fs <- GvHD[subset(pData(GvHD), Patient %in%5:7 & Visit %in% c(5:6))[["name"]]]
-
 ```
 
 ## 1d histogram/densityplot
-```{r}
+
+```r
 # histogram for raw flow data
 autoplot(fs, aes(x = `FL1-H`))
+```
 
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-1.png) 
+
+```r
 # add transformation
 autoplot(fs, aes(x = `FL1-H`)) + scale_x_log10()
+```
 
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-2.png) 
+
+```r
 # disable marginal events filtering
 autoplot(fs, aes(x = `FL1-H`), margin = F) + scale_x_log10()
+```
 
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-3.png) 
+
+```r
 # density
 autoplot(fs, aes(x = `FL1-H`), plotType = "density") + scale_x_log10()
+```
 
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-4.png) 
+
+```r
 # customize border colors 
 ggplot(fs, aes(x = `FL1-H`)) + facet_wrap(~name) + geom_histogram(colour = "white") + scale_x_log10()
+```
 
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-5.png) 
+
+```r
 # change the bin width
 ggplot(fs, aes(x = `FL1-H`)) + facet_wrap(~name, scale = "free") + geom_histogram(colour = "white", binwidth = 1/10) + scale_x_log10()
 ```
 
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-6.png) 
+
 ## 2d scatter/dot plot
-```{r}
+
+```r
 # default geom_hex plot
 autoplot(fs, aes(x = `FSC-H`, y =  `SSC-H`)) 
+```
 
+![plot of chunk unnamed-chunk-4](figure/unnamed-chunk-4-1.png) 
+
+```r
 # add contour
 autoplot(fs, aes(x = `FSC-H`, y =  `SSC-H`)) + geom_density2d(colour = "black")
+```
 
+![plot of chunk unnamed-chunk-4](figure/unnamed-chunk-4-2.png) 
+
+```r
 # change the faceting
 autoplot(fs, aes(x = `FSC-H`, y =  `SSC-H`)) + facet_grid(Patient~Visit) 
-
 ```
+
+![plot of chunk unnamed-chunk-4](figure/unnamed-chunk-4-3.png) 
 
