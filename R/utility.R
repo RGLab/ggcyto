312d03ef0d47f9c7bfd4639b98cf32d33a54608e
mikejiang <wjiang2@fhcrc.org>
2015-06-03 11:09:17 -0700

    use new feature of rbindlist from data.table 1.9.5

--- R/utility.R
+++ R/utility.R
@@ -3,45 +3,42 @@
 #' data.table version of ldply
 #' @param .data a list
 #' @return a data.table
-.ldply <- function (.data,  ..., .id = NA) 
+.ldply <- function (.data, .fun = NULL, ..., .id = NA) 
 {
-  index <- names(.data)
-  if(is.null(index)){
-    index <- 1:length(.data)
-    .id <- NULL
-  }
   
-  res <- .do_loop(index = index, .data = .data, ..., .id = .id)
-  res <- rbindlist(res)
-  setkeyv(res, .id)
+  if(is.null(names(.data)))
+    idcol <- NULL
+  else{
+    if(is.na(.id))
+      idcol <- ".id"
+    else
+      idcol <- .id
+  }
+
+  
+  res <- sapply(.data, .fun, ..., simplify = FALSE)
+  
+  res <- rbindlist(res, idcol = idcol)
+  if(!is.null(idcol))
+    setkeyv(res, .id)
   res
 }
 
-#' @param index the index of the list, can be either character or numeric
-#' @param  .fun the function to apply to each element of .data
-#' @param ... other arguments passed to .fun
-#' @param .id see help(ldply)
-.do_loop <- function(index, .data, .fun = NULL, ..., .id = NA){
-  lapply(index, function(i){
-        dt <- .fun(.data[[i]], ...)
-        #append id
-        if(!is.null(.id)){
-          if (is.na(.id)) {
-            .id <- ".id"
-          }
-          eval(substitute(dt[, newCol := i], list(newCol = .id)))
-        }
-        dt
-      })
-}
 #' convert a flowSet to a data.table
 #' @param .data flowSet
-.fsdply <- function (.data, ..., .id = NA) 
+.fsdply <- function (.data, .fun = NULL, ..., .id = NA) 
 {
-  index <- sampleNames(.data)
-  res <- .do_loop(index = index, .data = .data, ..., .id = .id)
-  res <- rbindlist(res)
-  setkeyv(res, .id)
+  
+  if(is.na(.id))
+    idcol <- ".id"
+  else
+    idcol <- .id
+
+  res <- fsApply(.data, .fun, ..., simplify = FALSE)
+  
+  res <- rbindlist(res, idcol = idcol)
+  if(!is.null(idcol))
+    setkeyv(res, .id)
   res
 }
 
