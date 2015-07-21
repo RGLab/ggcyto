dbbdd8508f5aae6569d3dea12ef2eba966768603
mikejiang <wjiang2@fhcrc.org>
2015-07-21 16:43:38 -0700

    add VignetteIndexEntry to Rmd files

--- R/getFlowFrame.R
+++ R/getFlowFrame.R
@@ -4,19 +4,23 @@
 #' 
 #' @param x flowSet or GatingSet/GatingHierarchy
 #' @return an empty flowFrame
+#' @export
 getFlowFrame <- function(x)UseMethod("getFlowFrame")
 
 #' @rdname getFlowFrame
+#' @export
 getFlowFrame.flowSet <- function(x){
   x[[1, use.exprs = FALSE]]
 }
 
 #' @rdname getFlowFrame
+#' @export
 getFlowFrame.GatingSet <- function(x){
   getFlowFrame(getData(x))
 }
 
 #' @rdname getFlowFrame
+#' @export
 getFlowFrame.GatingHierarchy <- function(x){
   getData(x, use.exprs = FALSE)
 }
