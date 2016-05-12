00e136d6473f6cf78fabb2817b1ce08146378e28
Mike Jiang <wjiang2@fhcrc.org>
2016-05-12 23:21:33 +0000

    pass on dots to arrangeGrob in print.GatingSet_layout function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggcyto@117425 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ggcyto_GatingLayout.R
+++ R/ggcyto_GatingLayout.R
@@ -1,34 +1,34 @@
 #' print method for ggcyto_gate_layout class
 #' It calls arrangeGrob to arrange a list of ggplot objects stored as ggcyto_gate_layout object
 #' @param x ggcyto_gate_layout, which is essentially a list of ggplot objects
-#' @param ... not used
+#' @param ... other arguments passed to arrangeGrob
 #' @export
 #' @return nothing
 #' @method print ggcyto_GatingLayout
 print.ggcyto_GatingLayout <- function(x, ...){
-  
+
   #must do the conversion to ggplot here since + require ggcyto object
   for(j in seq_along(x)){
     # browser()
     p <- x[[j]]
     popName <- attr(p$data, "strip.text")
     p <- as.ggplot(p)
-    
+
     p$data[, name:= popName]
-    
+
     for(i in seq_along(p$layers)){
       if(!ggplot2:::is.waive(p$layers[[i]][["data"]])){
-        
+
         p$layers[[i]][["data"]][, name:= popName]
       }
-      
+
     }
-    
+
     x[[j]] <- p
   }
-  
+
   arrange.main <- x@arrange.main
-  plot(do.call(arrangeGrob, c(grobs = x, top = arrange.main)))  
+  plot(do.call(arrangeGrob, c(grobs = x, top = arrange.main, ...)))
 }
 
 #' @param object ggcyto_GatingLayout
@@ -44,14 +44,14 @@ setMethod("show", "ggcyto_GatingLayout", show.ggcyto_GatingLayout)
 
 
 #' overloaded '+' method for ggcyto_gate_layout
-#' 
+#'
 #' It adds the layer specified by 'e2' to each individual ggplot object stored in ggcyto_gate_layout
 #' @param e1 ggcyto_gate_layout
 #' @param e2 any ggplot layer
 #' @return a modified ggcyto_gate_layout object
 #' @export
 #' @return a GatingLayout object
-#' @examples 
+#' @examples
 #' #autplot for GatingSet
 #' dataDir <- system.file("extdata",package="flowWorkspaceData")
 #' gs <- load_gs(list.files(dataDir, pattern = "gs_manual",full = TRUE))
@@ -61,7 +61,7 @@ setMethod("show", "ggcyto_GatingLayout", show.ggcyto_GatingLayout)
 #' # customize the font size of strip text for each ggcyo plots contained in GatingLayout object
 #' p + theme(strip.text = element_text(size = 14))
 `+.ggcyto_GatingLayout` <- function(e1, e2){
-  
+
   for(i in seq_along(e1)){
     e1[[i]] <- e1[[i]] + e2
   }
