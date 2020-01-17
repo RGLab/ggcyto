b7dfb017791907d3d3aed7663e252449216154a8
mikejiang <wjiang2@fhcrc.org>
2020-01-17 10:40:44 -0800

    transparent --> NA

--- R/geom_gate.R
+++ R/geom_gate.R
@@ -12,7 +12,7 @@
 #' @aliases geom_gate.default geom_gate.list geom_gate.filter geom_gate.filterList
 #' geom_gate.quadGate geom_gate.character geom_gate.filters geom_gate.filtersList
 #' geom_gate.logical geom_gate.logicalFilterResult
-#' @usage geom_gate(data, mapping = NULL, fill = "transparent", colour = "red", nPoints = 100, ...)
+#' @usage geom_gate(data, mapping = NULL, fill = NA, colour = "red", nPoints = 100, ...)
 #' @param data a filter (Currently only rectangleGate (1d or 2d), polygonGate, ellipsoidGate are supported.)
 #'              or a list of these gates 
 #'              or filterList
@@ -96,7 +96,7 @@ geom_gate.filterList <- function(data, ...){
 }
 
 #' @export
-geom_gate.filter <- function(data, mapping = NULL, fill = "transparent", colour = "red", nPoints = 100, ...){
+geom_gate.filter <- function(data, mapping = NULL, fill = NA, colour = "red", nPoints = 100, ...){
   structure(
     list(filter = data
          , gate_params = list(mapping = mapping
@@ -110,7 +110,7 @@ geom_gate.filter <- function(data, mapping = NULL, fill = "transparent", colour 
 }
 
 
-geom_gate_impl.polygonGate <- function(data, mapping = NULL, fill = "transparent", colour = "red", nPoints = 100, ...){
+geom_gate_impl.polygonGate <- function(data, mapping = NULL, fill = NA, colour = "red", nPoints = 100, ...){
   
   #To proper interpolate the polygon we need to pass nPoints
   # so we need to avoid the fority process triggered by geom_path$new here (by not passing the data)
@@ -123,7 +123,7 @@ geom_gate_impl.polygonGate <- function(data, mapping = NULL, fill = "transparent
 }
 
 
-geom_gate_impl.rectangleGate <- function(data, mapping = NULL, fill = "transparent", colour = "red", nPoints = 100, ...){
+geom_gate_impl.rectangleGate <- function(data, mapping = NULL, fill = NA, colour = "red", nPoints = 100, ...){
   
   param <- parameters(data)
   nDim <- length(param)
