9888b4b5652b77ae0901723f98d82aed57cba294
Teun van den Brand <tahvdbrand@gmail.com>
2025-07-02 10:48:26 +0200

    update classes based on S3/S7 classes

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,7 +1,14 @@
-setOldClass(c("gg", "ggplot"))
+if (exists("class_ggplot", envir = asNamespace("ggplot2"))) {
+  # Should equal `S7:::class_dispatch(class_ggplot)`
+  ggplot_class <- c("ggplot2::ggplot", "ggplot2::gg", "S7_object")
+} else {
+  ggplot_class <- c("gg", "ggplot")
+}
+setOldClass(ggplot_class)
+
 #' @export
 #' @rdname ggcyto
-setClass("ggcyto", contains = c("gg", "ggplot"))
+setClass("ggcyto", contains = ggplot_class)
 #' @export
 #' @rdname ggcyto
 setClass("ggcyto_flowSet", contains = "ggcyto")
