ce1428f0121921fa20d0a6ae4129e70ab56a2faf
Teun van den Brand <tahvdbrand@gmail.com>
2025-07-02 11:08:30 +0200

    conditionally set coercion methods

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,4 +1,6 @@
-if (exists("class_ggplot", envir = asNamespace("ggplot2"))) {
+ggplot_is_S7 <- exists("class_ggplot", envir = asNamespace("ggplot2"))
+
+if (ggplot_is_S7) {
   # Should equal `S7:::class_dispatch(class_ggplot)`
   ggplot_class <- c("ggplot2::ggplot", "ggplot2::gg", "S7_object")
 } else {
@@ -18,4 +20,24 @@ setClass("ggcyto_GatingSet", contains = "ggcyto_flowSet")
 #' @export
 #' @rdname ggcyto
 setClass("ggcyto_GatingLayout", contains = "list", slots = c(arrange.main = "character"))
+
+if (ggplot_is_S7) {
+  setAs(
+    from = "ggplot2::ggplot",
+    to = "ggcyto_flowSet",
+    function(from) {
+      class(from) <- union(c("ggcyto_flowSet", "ggcyto"), class(from))
+      from
+    }
+  )
+
+  setAs(
+    from = "ggcyto_flowSet",
+    to = "ggcyto_GatingSet",
+    function(from) {
+      class(from) <- union("ggcyto_GatingSet", class(from))
+      from
+    }
+  )
+}
 
